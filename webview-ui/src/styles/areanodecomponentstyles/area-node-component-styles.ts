05540d85aaa477c7abca3e7c31a20dda313c5611
Omobolaji showemimo <81483565+1GASAKING@users.noreply.github.com>
2026-08-15 19:39:02 -0700

    complete resizing

--- webview-ui/src/styles/areanodecomponentstyles/area-node-component-styles.ts
+++ webview-ui/src/styles/areanodecomponentstyles/area-node-component-styles.ts
@@ -28,6 +28,46 @@ const AreaNodeComponentMainDiv = styled.div<AreaNodeComponentMainDivProp>`
       display: flex;
     }
   }
+  .react-flow__resize-control.line {
+    box-sizing: border-box;
+    
+  }
+  .react-flow__resize-control.line.left {
+    left: -6px;
+    top: 0;
+    height: 100%;
+    width: 7px;
+    transform: none;
+    border-left-width: 0;
+    border-right-width: 1px;
+  }
+  .react-flow__resize-control.line.right {
+    left: 100%;
+    top: 0;
+    height: 100%;
+    width: 7px;
+    transform: none;
+    border-right-width: 0;
+    border-left-width: 1px;
+  }
+  .react-flow__resize-control.line.top {
+    top: -6px;
+    left: 0;
+    width: 100%;
+    height: 7px;
+    transform: none;
+    border-top-width: 0;
+    border-bottom-width: 1px;
+  }
+  .react-flow__resize-control.line.bottom {
+    top: 100%;
+    left: 0;
+    width: 100%;
+    height: 7px;
+    transform: none;
+    border-bottom-width: 0;
+    border-top-width: 1px;
+  }
   .area-label {
     max-width: 100%;
     padding: 8px 8px;
