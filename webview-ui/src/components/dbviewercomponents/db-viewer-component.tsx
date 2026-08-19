ba93e41e7af2573c0118861f525205e23dbcd968
Omobolaji showemimo <81483565+1GASAKING@users.noreply.github.com>
2026-08-19 01:30:36 -0700

    me stop

--- webview-ui/src/components/dbviewercomponents/db-viewer-component.tsx
+++ webview-ui/src/components/dbviewercomponents/db-viewer-component.tsx
@@ -1,8 +1,79 @@
-import { DBViewerComponentMianDiv } from "../../styles/dbviewcomponentsstyles/dbviewercomponentstyles"
+import { DBViewerComponentBodyDiv, DBViewerComponentHeaderDiv, DBViewerComponentMianDiv } from "../../styles/dbviewcomponentsstyles/dbviewercomponentstyles"
+import { VsButton } from "../../styles/reusablecomponentsstyles/button-component-styles"
 
 const DBViewerComponent =()=>{
     return(
         <DBViewerComponentMianDiv>
+            <div>
+                <DBViewerComponentHeaderDiv>
+                    <div>
+                        <div>
+                            <div> Table Name </div>
+                            <div> description about all rows showed </div>
+
+                        </div>
+                        <div className="left-items">
+                            
+                            <div className="db-viewer-action-button ">
+                                <VsButton>
+                                    options 
+
+                                    
+                                    
+                                </VsButton>
+                            </div>
+                            
+                            <div className="db-viewer-action-button ">
+                                <VsButton>
+                                    undo 
+
+                                    
+                                    
+                                </VsButton>
+                            </div>
+                            <div className="db-viewer-action-button ">
+                                <VsButton>
+                                    redo 
+
+                                    
+                                    
+                                </VsButton>
+                            </div>
+                            
+                            <div className="db-viewer-action-button ">
+                                <VsButton>
+                                    save 
+
+                                    
+                                    
+                                </VsButton>
+                            </div>
+
+                            <div>
+                                <VsButton>
+                                    new 
+
+                                    <span>
+                                        /
+                                    </span>
+                                </VsButton>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </DBViewerComponentHeaderDiv>
+                <DBViewerComponentBodyDiv>
+                    <div>
+                        <div>
+                            headers
+
+                        </div>
+                    </div>
+                </DBViewerComponentBodyDiv>
+
+            </div>
+
 
         </DBViewerComponentMianDiv>
     )
--- webview-ui/src/styles/dbviewcomponentsstyles/dbviewercomponentstyles.ts
+++ webview-ui/src/styles/dbviewcomponentsstyles/dbviewercomponentstyles.ts
@@ -1,7 +1,29 @@
 import styled from "styled-components";
 
-const DBViewerComponentMianDiv = styled.div``
+const DBViewerComponentMianDiv = styled.div``;
+
+const DBViewerComponentHeaderDiv = styled.div`
+padding:8px 10px;
+  > div {
+    display: flex;
+    align-items: end;
+    justify-content: space-between;
+  }
+  .left-items {
+    display: flex;
+    gap: 4px;
+  }
+  .db-viewer-action-button {
+    display: flex;
+    align-iems: center;
+    height: 28px;
+  }
+`;
+
+const DBViewerComponentBodyDiv = styled.div``;
 
 export {
-    DBViewerComponentMianDiv,
-}
+  DBViewerComponentMianDiv,
+  DBViewerComponentHeaderDiv,
+  DBViewerComponentBodyDiv,
+};
